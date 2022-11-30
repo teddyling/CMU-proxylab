0a41548eea6f903906ffe9d61c7e451ac429cf73
Taichen Ling <taichenl@angelshark.ics.cs.cmu.edu>
2022-11-30 01:04:41 -0500

    Just get started, completed the main function

--- proxy.c
+++ proxy.c
@@ -43,6 +43,10 @@
  */
 #define MAX_CACHE_SIZE (1024 * 1024)
 #define MAX_OBJECT_SIZE (100 * 1024)
+#define HOSTLEN 256
+#define PORTLEN 8
+
+typedef struct sockaddr SA;
 
 /*
  * String to use for the User-Agent header.
@@ -53,6 +57,37 @@ static const char *header_user_agent = "Mozilla/5.0"
                                        " Gecko/20221116 Firefox/63.0.1";
 
 int main(int argc, char **argv) {
-    printf("%s", header_user_agent);
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s <poart>\n", argv[0]);
+        exit(1);
+    }
+
+    int connfd;
+    socklen_t clientlen;
+    struct sockaddr_in clientaddr;
+    char client_hostname[HOSTLEN];
+    char client_port[PORTLEN];
+    int listenfd = open_listenfd(argv[1]);
+    if (listenfd < 0) {
+        fprintf(stderr, "Failed to listen on port: %s\n", argv[1]);
+        exit(1);
+    }
+    while (true) {
+        clientlen = sizeof(struct sockaddr_storage);
+        connfd = accept(listenfd, (SA *)&clientaddr, &clientlen);
+        if (connfd < 0) {
+            perror("accept");
+            continue;
+        }
+        int gni = getnameinfo((SA *)&clientaddr, clientlen, client_hostname,
+                              HOSTLEN, client_port, PORTLEN, 0);
+        if (gni == 0) {
+            printf("Accepted connection from: %s:%s\n", client_hostname,
+                   client_port);
+        } else {
+            fprintf(stderr, "getnameinfo failed: %s\n", gai_strerror(gni));
+        }
+    }
+
     return 0;
 }
